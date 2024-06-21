9128b0525881c5386d5a4c783b9aa9c9779f09eb
nothing <161198246+PYHOMES00@users.noreply.github.com>
2024-06-21 21:09:37 +0530

    Update commands.py

--- clone_plugins/commands.py
+++ clone_plugins/commands.py
@@ -52,12 +52,13 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('💝 sᴜʙsᴄʀɪʙᴇ ᴍʏ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ', url='https://youtube.com/@Tech_VJ')
+            InlineKeyboardButton('◤ᴜᴘᴅᴀᴛᴇ☯ɢʀᴏᴜᴘ◢', url='https://t.me/mr_persis_bot'),
+            InlineKeyboardButton('◤ꜱᴜᴘᴘᴏʀᴛ☯ɢʀᴏᴜᴘ◢', url='https://t.me/mr_persis_Support_group')
             ],[
             InlineKeyboardButton('🤖 ᴄʀᴇᴀᴛᴇ ʏᴏᴜʀ ᴏᴡɴ ᴄʟᴏɴᴇ ʙᴏᴛ', url=f'https://t.me/{BOT_USERNAME}?start=clone')
             ],[
-            InlineKeyboardButton('💁‍♀️ ʜᴇʟᴘ', callback_data='help'),
-            InlineKeyboardButton('ᴀʙᴏᴜᴛ 🔻', callback_data='about')
+            InlineKeyboardButton('◤ʜᴇʟᴘ◢', callback_data='help'),
+            InlineKeyboardButton('◤ᴀʙᴏᴜᴛ◢', callback_data='about')
         ]]
         me2 = (await client.get_me()).mention
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -180,12 +181,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.message.delete()
     elif query.data == "start":
         buttons = [[
-            InlineKeyboardButton('💝 sᴜʙsᴄʀɪʙᴇ ᴍʏ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ', url='https://youtube.com/@Tech_VJ')
+            InlineKeyboardButton('◤ᴜᴘᴅᴀᴛᴇ☯ɢʀᴏᴜᴘ◢', url='https://t.me/mr_persis_bot'),
+            InlineKeyboardButton('◤ꜱᴜᴘᴘᴏʀᴛ☯ɢʀᴏᴜᴘ◢', url='https://t.me/mr_persis_Support_group')
             ],[
             InlineKeyboardButton('🤖 ᴄʀᴇᴀᴛᴇ ʏᴏᴜʀ ᴏᴡɴ ᴄʟᴏɴᴇ ʙᴏᴛ', url=f'https://t.me/{BOT_USERNAME}?start=clone')
             ],[
-            InlineKeyboardButton('💁‍♀️ ʜᴇʟᴘ', callback_data='help'),
-            InlineKeyboardButton('ᴀʙᴏᴜᴛ 🔻', callback_data='about')
+            InlineKeyboardButton('◤ʜᴇʟᴘ◢', callback_data='help'),
+            InlineKeyboardButton('◤ᴀʙᴏᴜᴛ◢', callback_data='about')
         ]]
         
         reply_markup = InlineKeyboardMarkup(buttons)
